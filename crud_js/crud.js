7e6e7521a00100cc83de35b6a3706bf55cde5684
Youssef Tabouda <ytabouda@smb.sqli.com>
2023-08-28 15:45:46 +0100

    search data by title & category

--- crud_js/crud.js
+++ crud_js/crud.js
@@ -35,14 +35,14 @@ if(localStorage.product != null){
 
 submit.onclick = function (){
     let newPro = {
-        title:title.value,
+        title:title.value.toLowerCase(),
         price:price.value,
         taxes:taxes.value,
         ads:ads.value,
         discount:discount.value,
         total:total.innerHTML,
         count:count.value,
-        category:category.value,
+        category:category.value.toLowerCase(),
     }
     
     
@@ -163,12 +163,66 @@ function updateData(i)
 
 
 
+//Search
+let SearchMood = 'title';
+function getSearchMood(id)
+{
+    let search = document.getElementById('search');
+    if(id == 'searchTitle'){
+        SearchMood = 'title';
+    }else{
+        SearchMood = 'category';
+    }
+    search.placeholder = 'search by ' + SearchMood;
+    search.focus();
+    search.value = '';
+    showData();
+}
 
-// ...
+function searchData(value)
+{
+    let table = '';
 
+    for(let i=0; i < dataPro.length; i++){
+        if(SearchMood == 'title'){
+        
+                if(dataPro[i].title.includes(value.toLowerCase())){
+                    table += 
+            `<tr>
+                <td>${i}</td>
+                <td>${dataPro[i].title}</td>    
+                <td>${dataPro[i].price}</td>
+                <td>${dataPro[i].taxes}</td>
+                <td>${dataPro[i].ads}</td>
+                <td>${dataPro[i].discount}</td>
+                <td>${dataPro[i].total}</td>
+                <td>${dataPro[i].category}</td>
+                <td><button onclick="updateData( ${i} )" id="update">update</button></td>
+                <td><button onclick="deleteData( ${i} )" id="delete">delete</button></td>
+            </tr>` ;
+                }
 
+        }else{
+                if(dataPro[i].category.includes(value.toLowerCase())){
+                    table += 
+            `<tr>
+                <td>${i}</td>
+                <td>${dataPro[i].title}</td>    
+                <td>${dataPro[i].price}</td>
+                <td>${dataPro[i].taxes}</td>
+                <td>${dataPro[i].ads}</td>
+                <td>${dataPro[i].discount}</td>
+                <td>${dataPro[i].total}</td>
+                <td>${dataPro[i].category}</td>
+                <td><button onclick="updateData( ${i} )" id="update">update</button></td>
+                <td><button onclick="deleteData( ${i} )" id="delete">delete</button></td>
+            </tr>` ;
+                }
 
-
+        }
+    }
+    document.getElementById('tbody').innerHTML = table;
+}
 
 
 
